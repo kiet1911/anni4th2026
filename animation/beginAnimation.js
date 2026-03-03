74100b94d69da009b7e6b482665475aa62e611e1
kenlyer <nguyentuankiet19112002@gmail.com>
2026-03-04 00:21:20 +0700

    add letter

--- animation/beginAnimation.js
+++ animation/beginAnimation.js
@@ -21,7 +21,42 @@ function animateBox() {
 
     requestAnimationFrame(step);
 }
+//letter
+function letterActive() {
+    const envelope = document.querySelector('.envelope');
+    // const photoss = document.querySelector('.frame-container');
+    console.log(envelope);
+    envelope.style.opacity = 1;
+    document.body.opacity = 0.4;
+    // photoss.style.opacity = 1;
+    setTimeout(() => {
+        envelope.classList.add('active');
+        // Sau khi mở thư thì chạy typing effect
+        setTimeout(() => {
+            const text = "To my love Kim Ngân,\n\nHappy Birthday, cutie! May your cake be as sweet as you, and may your smile today shine brighter than all the candles. 🎂✨";
+            const target = document.getElementById("letter-text");
+            let i = 0;
+            const speed = 50; // tốc độ chữ (ms)
+            function typeWriter() {
+                if (i < text.length) {
+                    target.textContent += text.charAt(i);
+                    i++;
+                    setTimeout(typeWriter, speed);
+                }
+            }
+            typeWriter();
+        }, 3000); // chờ animation mở thư xong
+    }, 7000);
+}
+
+
+
 //count before fly 
 setTimeout(() => {
     animateBox();
+    // letterActive();
 }, 3000);
+
+//letter
+
+
